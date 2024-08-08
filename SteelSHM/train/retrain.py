9c9be42e6e678001a165a04b21fb05b247068769
@GL_TanyuD <derick@uni-bremen.de>
2024-08-08 21:16:28 +0200

    Lets  begin

---
+++ SteelSHM/train/retrain.py
@@ -0,0 +1,221 @@
+DEVICE = '1'
+
+import numpy as np
+import time
+from pyDOE import lhs
+
+import shutil
+import math
+
+# from silence_tensorflow import silence_tensorflow
+# silence_tensorflow()
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+# Setup GPU for training
+
+
+import tensorflow.compat.v1 as tf # type: ignore
+tf.disable_v2_behavior()
+import tensorflow as tf1
+# import deepxde as dde
+# import tensorflow_estimator as estimator
+
+import wandb # type: ignore
+wandb.require("core")
+# wandb.init(config=tf.flags.FLAGS, sync_tensorboard=True)
+
+from models import *
+from utils import *
+from utils_shm import *
+
+import time
+
+os.environ['CUDA_VISIBLE_DEVICES'] = DEVICE
+np.random.seed(1111)
+tf.set_random_seed(1111)
+
+
+# tf.get_logger().setLevel('NONE')
+# tf.autograph.set_verbosity(3)
+# tf.logging.set_verbosity(tf.logging.ERROR)
+# tf.logging.set_verbosity(tf.logging.ERROR)
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# Note: TensorFlow 1.10 version is used
+
+time_var = '20240808-103639'
+run_suffix = "Z_end"
+run_num = 4000
+project_name = "PINN-elastodynamics-SteelSHM_train"
+run_id = "w75fee74"
+
+if __name__ == "__main__":
+
+    #### Note: The detailed description for this case can be found in paper:
+    #### Physics informed deep learning for computational elastodynamicswithout labeled data.
+    #### https://arxiv.org/abs/2006.08472
+    #### But network configuration might be slightly different from what is described in paper.
+    PI = math.pi
+    nondim = True
+
+    w = 0.3
+    h = 0.004
+    in_size = 9529
+    in_size = 9529
+    lr_size = 512 #1024#128
+    tb_size = 512 #int(lr_size/2)
+
+    rho = 7.9e3 #7900
+    E = 1.8e11 #180e9
+    nu = 0.3
+    mu0 = E/(2*(1+nu))
+    lambda0 = E*nu/((1+nu)*(1-2*nu))
+
+    V0 = 1e-12 #1e-8
+    f0 = 120e3
+    n0 = 5
+    t_tot = 4.167e-5
+    tmax = 2.083e-4  
+
+    T = tmax  
+
+    # Domain bounds for x, y and t
+    lb = np.array([0, 0, 0.0])
+    ub = np.array([w, h, tmax])
+
+    # Network configuration
+    uv_layers   = [3] + 8 * [70] + [5]
+    dist_layers = [3] + 4 * [20] + [5]
+    part_layers = [3] + 4 * [20] + [5]
+
+    # Number of frames for postprocessing
+
+    # Initial condition point for u, v
+    IC = lb + np.array([w, h, 0.0]) * lhs(3, 5000)
+
+    # Collocation point for equation residual
+    XYT_c = lb + (ub - lb) * lhs(3, 7000)
+    
+    # P1 = np.array([0, 0, 0.0])
+    # P2 = np.array([0, h, 0.0])
+    # P3 = np.array([w, h/2, 0.0])
+    dense = 10
+    XYT_P1 = np.array([0        , 0          , 0]) + np.array([w/dense, h/dense  , T]) * lhs(3, 4000) 
+    XYT_P2 = np.array([0        , h  -h/dense, 0]) + np.array([w/dense, h/dense  , T]) * lhs(3, 4000) 
+    XYT_P3 = np.array([w-w/dense, h/2-h/dense, 0]) + np.array([w/dense, 2*h/dense, T]) * lhs(3, 8000) 
+
+    XYT_c = np.concatenate((XYT_c, XYT_P1, XYT_P2, XYT_P3), 0)
+
+    LW = np.array([0, 0, 0]) + np.array([w, 0, T]) * lhs(3, 8000)
+    UP = np.array([0, h, 0]) + np.array([w, 0, T]) * lhs(3, 8000)
+    LF = np.array([0, 0, 0]) + np.array([0, h, T]) * lhs(3, 8000)
+    RT = np.array([w, 0, 0]) + np.array([0, h, T]) * lhs(3, 8000)
+
+    P1 = np.array([0, 0  , 0]) + np.array([0, 0, T]) * lhs(3, 4000)
+    P2 = np.array([0, h  , 0]) + np.array([0, 0, T]) * lhs(3, 4000)
+    P3 = np.array([w, h/2, 0]) + np.array([0, 0, T]) * lhs(3, 4000)
+
+    t_P1 = P1[:, 2:3]
+    t_P2 = P2[:, 2:3]
+
+    v_P1 = f_exc(P1[:,1:2], P1[:,2:3])
+    v_P2 = f_exc(P2[:,1:2], P2[:,2:3])
+    # v_P3 = np.zeros_like(P1[:,1:2])
+
+    # u_P1 = np.zeros_like(P1[:,1:2])
+    # u_P2 = np.zeros_like(P2[:,1:2])
+    # u_P3 = np.zeros_like(P1[:,1:2])
+
+    P1 = np.concatenate((P1, v_P1), 1)
+    P2 = np.concatenate((P2, v_P2), 1)
+
+    # Add some boundary points into the collocation point set
+    XYT_c = np.concatenate((XYT_c, LF[::5, :], RT[::5, :], UP[::5, :], LW[::5, :], P1[::5, 0:3], P2[::5, 0:3], P3[::5, :]), 0)
+
+    # direct = '../output/'+time_var
+    # if not os.path.exists(direct):
+    #     os.makedirs(direct)
+    # pts_plot([XYT_c, IC, LW, UP, LF, RT, P1, P2, P3], direct)
+
+    if nondim == True:
+        # Nondimensionalization parameters
+        L_star = 0.3 
+        T_star = L_star*np.sqrt(rho/mu0) 
+        U_star = V0
+        S_star = rho*L_star*U_star/(T_star**2)
+
+        var_star = np.array([L_star, L_star, T_star])
+        var_star_U = np.array([L_star, L_star, T_star, U_star])
+        # Nondimensionalize coordinates and inflow velocity
+        XYT_c /= var_star 
+        P1 /= var_star_U
+        P2 /= var_star_U
+        P3 /= var_star 
+        IC /= var_star 
+        LF /= var_star 
+        RT /= var_star 
+        UP /= var_star 
+        LW /= var_star 
+        lb /= var_star 
+        ub /= var_star 
+        
+    else:
+        L_star = 1.0
+        T_star = 1.0
+        U_star = 1.0
+        S_star = 1.0
+
+    
+
+    # Generate distance function for spatio-temporal space
+    x_dist, y_dist, t_dist = GenDistPt(xmin=0, xmax=w/L_star, ymin=0, ymax=h/L_star, tmin=0, tmax=tmax/T_star,
+                                       num=21, num_t=21)
+    XYT_dist = np.concatenate((x_dist, y_dist, t_dist), 1)
+    DIST = GenDist(XYT_dist)
+    
+    with tf.device('/device:GPU:%s'%(DEVICE)):      
+
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        session = tf.Session(config=config)
+        # wandb.tensorflow.log(tf.summary.merge_all())  
+        
+        direct = '../PINN_result/'+time_var
+        if not os.path.exists(direct):
+            os.makedirs(direct)
+
+        # Provide directory (second init) for pretrained networks if you have
+        model = PINN(XYT_c, P1, P2, P3, IC, LF, RT, UP, LW, DIST, 
+                     uv_layers, dist_layers, part_layers, 
+                     lb, ub, 
+                     rho, mu0, lambda0, 
+                     L_star, T_star, U_star, S_star, 
+                     direct,
+                     partDir='/partNN_float64.pickle', 
+                     distDir='/distNN_float64.pickle', 
+                     uvDir='/zAdam2_uvNN_float64.pickle'#'/uvNN_float64.pickle_'+run_suffix 
+                    , run_num = run_num)
+        # model = PINN(XYT_c, HOLE, IC, LF, RT, UP, LW, DIST, uv_layers, dist_layers, part_layers, lb, ub,
+        #                 partDir='./partNN_float64.pickle', distDir='./distNN_float64.pickle', uvDir='uvNN_float64.pickle')
+                
+
+        # Train the composite network
+        start_time = time.time()
+        run = wandb.init(project=project_name, id=run_id, resume="must")#name = wandb_name+'_Gen')
+        # print('Training General function')
+        # model.train(iter=2000, learning_rate=1e-3)
+        # model.save_NN('%s/uvNN_float64.pickle'%(direct), TYPE='UV')
+        # print('Training General function')
+        # model.train(iter=2000, learning_rate=5e-4)
+        # model.save_NN('%s/uvNN_float64.pickle'%(direct), TYPE='UV')
+        print('Training General function')
+        model.train_bfgs()
+        model.save_NN('%s/uvNN_float64.pickle'%(direct), TYPE='UV')
+        print("--- %s seconds ---" % (time.time() - start_time))
+        print()
+        # wandb.finish()
+
+        # Save the trained model
+
+
+        # Check the loss for each part
+        model.getloss()
---
+++ SteelSHM/train/utils_shm.py
@@ -0,0 +1,179 @@
+import numpy as np
+
+dir_path = '/home/derick/Documents/PINNS/examples/mfl_src/'
+
+w = 0.3
+h = 0.004
+in_size = 9529
+in_size = 9529
+lr_size = 512 #1024#128
+tb_size = 512 #int(lr_size/2)
+
+rho = 7.9e3 #7900
+E = 1.8e11 #180e9
+nu = 0.3
+mu0 = E/(2*(1+nu))
+lambda0 = E*nu/((1+nu)*(1-2*nu))
+# mu0 = 1.8e11 #180e9
+# lambda0 = 0.3
+
+V0 = 1e-12 #1e-8
+f0 = 120e3
+n0 = 5
+t_tot = 4.167e-5
+tmax = 2.083e-4
+
+density = 10
+
+
+def hanning_window(t, T):
+    # return (torch.sin(math.pi * t / T))**2  
+    return 0.5 * (1 - np.cos(2 * np.pi * t / T))
+
+def hanning_windowed_sine_burst(t, A, fc, T):
+    # A       # Amplitude of the sine wave
+    # fc      # Frequency of the sine wave (in Hz)
+    # T       # Duration of the burst (in seconds)
+    return A * np.sin(2 * np.pi * fc * t) * hanning_window(t, T)
+
+def f_exc(z, t, sym=False, V=V0, f=f0, n=n0, t_tot=t_tot):
+    cut_off_val = hanning_windowed_sine_burst(np.array(t_tot), V, f, t_tot)
+    step_fn = np.heaviside(t_tot-t, cut_off_val)
+    # step_fn = 1
+
+    if sym:
+        # Ensure that excitation at f_exc(z=0,t) = -f_exc(z=h,t)
+        result = np.cos(np.pi*z/h)*hanning_windowed_sine_burst(t, V, f, t_tot)*step_fn
+    else:
+        result = hanning_windowed_sine_burst(t, V, f, t_tot)*step_fn
+
+    return result
+
+
+# class distance_fxn():
+#     def __init__(self, boundary):#, domain, details):
+#         # seed_everything(0, workers=True)
+
+#         self.batch_line_dist  = vmap(self.line_dist,(0, 0, 0))
+#         self.batch_plane_dist = vmap(self.plane_dist,(0, 0, 0))
+
+#         if boundary == 'u':
+#             self.max_val = tmax#w/2
+#             # x = 0, z = 0 & x = 0, z = h
+#             # x = w, z = h/2
+#             # t = 0
+#             # x = 0
+#             self.xtics = {'lines' :{'A':np.array([[0, 0, 0], [0, 0, h], [0, w, h/2]]),  
+#                                     'd':np.array([[1, 0, 0], [1, 0, 0], [1, 0, 0  ]])},
+#                           'planes':{'a':np.array([[1], [0]]), 
+#                                     'b':np.array([[0], [1]]), 
+#                                     'c':np.array([[0], [0]]), 
+#                                     'd':np.array([[0], [0]])}
+#                     }
+            
+#         if boundary == 'v':
+#             self.max_val = tmax#700e-6#1#tmax#w/2
+#             # x = 0, z = 0 & x = 0, z = h
+#             # x = w, z = h/2
+#             # t = 0
+#             self.xtics = {'lines' :{'A':np.array([[0, 0, 0], [0, 0, h], [0, w, h/2]]), 
+#                                     'd':np.array([[1, 0, 0], [1, 0, 0], [1, 0, 0  ]])},
+#                           'planes':{'a':np.array([[1]]), 
+#                                     'b':np.array([[0]]), 
+#                                     'c':np.array([[0]]), 
+#                                     'd':np.array([[0]])}
+#                          }
+
+#         if boundary == 'xx':
+#             self.max_val = w
+#             # x = w
+#             self.xtics = {'planes':{'a':np.array([[0]]), 
+#                                     'b':np.array([[1]]), 
+#                                     'c':np.array([[0]]), 
+#                                     'd':np.array([[w]])}
+#                          }
+        
+#         if boundary == 'yy':
+#             self.max_val = h/2
+#             # z = 0
+#             # z = h
+#             self.xtics = {'planes':{'a':np.array([[0], [0]]), 
+#                                     'b':np.array([[0], [0]]), 
+#                                     'c':np.array([[1], [1]]), 
+#                                     'd':np.array([[0], [h]])}
+#                          }
+        
+#         if boundary == 'xy':
+#             self.max_val = h/2
+#             # z = 0
+#             # z = h
+#             # x = 0
+#             # x = w
+#             self.xtics = {'planes':{'a':np.array([[0], [0], [0], [0]]), 
+#                                     'b':np.array([[0], [0], [1], [1]]), 
+#                                     'c':np.array([[1], [1], [0], [0]]), 
+#                                     'd':np.array([[0], [h], [0], [w]])}
+#                          }
+            
+#         self.boundary = boundary
+
+#     def line_dist(self, t, x, z):
+#         P = np.hstack((t, x, z))
+#         A = self.xtics['lines']['A']
+#         d = self.xtics['lines']['d']
+#         AP = P- A
+#         cross_product = np.cross(AP, d, axis =1)
+#         cross_product_magnitude = np.linalg.norm(cross_product, axis=1)
+#         direction_magnitude = np.linalg.norm(d, axis=1)
+
+#         distance = cross_product_magnitude / direction_magnitude
+#         # print(distance.shape)
+#         distance = np.min(distance, keepdims=True)
+#         # print(distance.shape)
+        
+#         return np.abs(distance)
+
+
+#     def plane_dist(self, t, x, z):
+#         a = self.xtics['planes']['a']
+#         b = self.xtics['planes']['b']
+#         c = self.xtics['planes']['c']
+#         d = self.xtics['planes']['d']
+#         num = np.abs(t*a + x*b + z*c - d)
+#         den = np.sqrt(a**2 + b**2 + c**2)
+
+#         distance = (num/den).reshape(-1, )
+#         # print(distance.shape)
+#         distance = np.min(distance, keepdims=True)
+#         # print(distance.shape)
+
+#         return np.abs(distance)
+
+#     def eval(self, t, x, y):
+#         if isinstance(x, (int, float, complex)):
+#             x = np.array(x)
+#         if isinstance(y, (int, float, complex)):
+#             y = np.array(y)
+#         if isinstance(t, (int, float, complex)):
+#             t = t*np.ones_like(x)
+
+#         t = t.reshape(-1, 1)
+#         x = x.reshape(-1, 1)
+#         y = y.reshape(-1, 1)
+        
+#         # A = np.hstack((t, x, y))
+#         distances = []
+
+#         key = list(self.xtics.keys())
+#         if 'lines' in key:
+#             distances.append(self.batch_line_dist(t, x, y))
+#         if 'planes' in key:
+#             distances.append(self.batch_plane_dist(t, x, y))
+
+#         distances = np.hstack(distances)
+
+#         distances = np.min(distances, axis=1, keepdims=True)
+#         # print(distances.shape)
+#         return  distances/self.max_val
+
+
